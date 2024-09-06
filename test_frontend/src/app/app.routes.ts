5f479bb8e6db81131a50190c0b760b2cade92608
DAHHANE Tarik <tarikdahhane@hotmail.fr>
2024-09-06 15:58:02 +0200

    Adding : Welcome page

--- test_frontend/src/app/app.routes.ts
+++ test_frontend/src/app/app.routes.ts
@@ -3,21 +3,24 @@ import { Routes } from '@angular/router';
 import { AuthGuardService } from './services/auth/user/auth-guard.service';
 import { NoAuthGuardService } from './services/auth/guest/no-auth-guard.service';
 
+import { WelcomeComponent } from './components/pages/welcome/welcome.component';
 import { HomeComponent } from './components/pages/home/home.component';
-import { ProfileComponent } from './components/pages/profile/profile/profile.component';
 import { SignInComponent } from './components/pages/auth/sign-in/sign-in.component';
 import { SignUpComponent } from './components/pages/auth/sign-up/sign-up.component';
 import { SignOutComponent } from './components/pages/auth/sign-out/sign-out.component';
+import { ProfileComponent } from './components/pages/profile/profile/profile.component';
 
 export const routes: Routes = [
     // Public routes
-    { path: '', component: HomeComponent },
+    
     
     // Non-authenticated routes
+    { path: 'welcome', component: WelcomeComponent, canActivate: [NoAuthGuardService] },
     { path: 'signIn', component: SignInComponent, canActivate: [NoAuthGuardService] },
     { path: 'signUp', component: SignUpComponent, canActivate: [NoAuthGuardService] },
 
     // Authenticated routes
+    { path: '', component: HomeComponent, canActivate: [AuthGuardService] },
     { path: 'signOut', component: SignOutComponent, canActivate: [AuthGuardService] },
     { path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService] },
 
